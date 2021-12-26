e49007961c7ef6f60886e886370f0e45351b2d1e
Allan Hortle <allanhortle@gmail.com>
2021-12-26 15:59:43 +1100

    Level 10.2

--- 10.ts
+++ 10.ts
@@ -8,14 +8,14 @@ const pairs: Record<string, string> = {
     '<': '>'
 };
 const opening = new Set(['(', '{', '[', '<']);
-const points: Record<string, number> = {
-    ')': 3,
-    ']': 57,
-    '}': 1197,
-    '>': 25137
-};
 
 function part1(data: string) {
+    const points: Record<string, number> = {
+        ')': 3,
+        ']': 57,
+        '}': 1197,
+        '>': 25137
+    };
     return data
         .split('\n')
         .map((row) => {
@@ -38,4 +38,41 @@ function part1(data: string) {
         .reduce((rr, ii) => rr + (ii ? points[ii] : 0), 0);
 }
 
-console.log({part1: part1(input)});
+function part2(data: string) {
+    const points: Record<string, number> = {
+        ')': 1,
+        ']': 2,
+        '}': 3,
+        '>': 4
+    };
+    const scores = data
+        .split('\n')
+        // process stacks
+        .flatMap((row) => {
+            const stack: string[] = [];
+            const fullStack: string[] = [];
+            for (const c of row.split('')) {
+                if (opening.has(c)) {
+                    stack.unshift(c);
+                    fullStack.unshift(c);
+                    continue;
+                }
+                if (c === pairs[stack[0]]) {
+                    stack.shift();
+                    continue;
+                }
+                return [];
+            }
+            return [stack];
+        })
+        // calculate scores
+        .map((stack) => {
+            return stack.reduce((rr, ii) => rr * 5 + points[pairs[ii]], 0);
+        })
+        .sort((aa, bb) => bb - aa);
+
+    return scores[Math.floor(scores.length / 2)];
+    //.reduce((rr, ii) => rr + (ii ? points[ii] : 0), 0);
+}
+
+console.log({part1: part1(input), part2: part2(input)});
