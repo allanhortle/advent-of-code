51d589413b1ede6cad7f36283ca4ccb78d99d0a1
Allan Hortle <allanhortle@gmail.com>
2021-12-28 16:16:40 +1100

    Level 12.2

--- 12.ts
+++ 12.ts
@@ -26,7 +26,56 @@ function part1(data: string) {
     }
 
     visit(['start']);
-    console.log(paths, paths.size);
+    return paths.size;
 }
 
-console.log({part1: part1(input)});
+function part2(data: string) {
+    const graph: Record<string, string[]> = {};
+    data.split('\n').forEach((row) => {
+        const [start, end] = row.split('-');
+        graph[start] ||= [] as string[];
+        graph[end] ||= [] as string[];
+        graph[start].push(end);
+        graph[end].push(start);
+    });
+
+    const paths = new Set();
+    const smallCaves = Object.keys(graph).filter(
+        (ii) => ii === ii.toLowerCase() && ii !== 'start' && ii !== 'end'
+    );
+    console.log(smallCaves);
+
+    function visit(path: string[]): any {
+        const key = path[path.length - 1];
+
+        return graph[key].map((target) => {
+            if (target === 'start') return;
+            if (target === 'end') {
+                paths.add(path.concat('end').join(','));
+                return;
+            }
+            if (target.toLowerCase() === target) {
+                const caveCounts = path
+                    .filter((ii) => smallCaves.includes(ii))
+                    .reduce((rr, key) => {
+                        rr[key] ||= 0;
+                        rr[key]++;
+                        return rr;
+                    }, {} as Record<string, number>);
+                if (
+                    caveCounts[target] > 1 ||
+                    (caveCounts[target] === 1 && Object.values(caveCounts).some((ii) => ii > 1))
+                ) {
+                    return;
+                }
+            }
+            return visit(path.concat(target));
+        });
+    }
+
+    visit(['start']);
+    //console.log(paths);
+    return paths.size;
+}
+
+console.log({part1: part1(input), part2: part2(input)});
